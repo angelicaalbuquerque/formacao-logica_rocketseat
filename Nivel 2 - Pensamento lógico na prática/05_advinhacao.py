dea471252ffca16a5186aef709676196c4de0b9c
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2026-03-10 15:35:22 -0300

    Adds guess game

--- Nivel 2 - Pensamento lógico na prática/05_advinhacao.py
+++ Nivel 2 - Pensamento lógico na prática/05_advinhacao.py
@@ -1,1 +1,19 @@
-# arrays
+#  Aprendemos a aplicar variáveis, listas e estruturas condicionais 
+
+perguntas = [ 
+  ['Seu animal gosta de bananas?', 'macaco'], 
+  ['Seu animal gosta de cenoura?', 'coelho'],
+  ['Seu animal gosta de queijo?', 'rato'], 
+  ['Seu animal gosta de bambu?', 'panda'], 
+]
+
+print('Pense em um animal...')
+
+#para cada elemento da lista de perguntas, associo à variável pergunta
+for pergunta in perguntas:
+  resposta = input(f'{pergunta[0]} (s/n): ')
+  if resposta.lower() == 's':
+    print(f'Você pensou em {pergunta[1]}!')
+    break
+else:
+  print('Poxa... não consegui adivinhar!')
