64446c528e3071fd2457ff6518db2b121a2c77b3
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2026-03-10 15:54:32 -0300

    Refactoring guess game

--- Nivel 2 - Pensamento lógico na prática/05_advinhacao.py
+++ Nivel 2 - Pensamento lógico na prática/05_advinhacao.py
@@ -7,13 +7,28 @@ perguntas = [
   ['Seu animal gosta de bambu?', 'panda'], 
 ]
 
-print('Pense em um animal...')
 
-#para cada elemento da lista de perguntas, associo à variável pergunta
-for pergunta in perguntas:
-  resposta = input(f'{pergunta[0]} (s/n): ')
-  if resposta.lower() == 's':
-    print(f'Você pensou em {pergunta[1]}!')
+while True:
+  print('Pense em um animal...')
+
+  acertou = False
+
+  for pergunta in perguntas:
+    resposta = input(f'{pergunta[0]} (s/n): ')
+    if resposta.lower() == 's':
+      print(f'Você pensou em {pergunta[1]}!')
+      acertou = True
+      break
+
+  if not acertou:
+    animal = input('Desisto! Em qual animal você pensou? ')
+    novaPergunta = input('Qual pergunta você faria para identificar esse animal? ')
+    perguntas.append([novaPergunta, animal])
+
+  resposta = input('Quer jogar de novo? [s/n]: ')
+  if resposta.lower() != 's':
+    print('Foi bom jogar com você!')
     break
-else:
-  print('Poxa... não consegui adivinhar!')
+
+
+
