56c82b4bd20c4155b25aaf54e4afe1e5d75869bb
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2026-06-02 17:38:52 -0300

    Adds fundamentals files

---
+++ Nível 1 - Fundamentos/02_variaveis-tipos.py
@@ -0,0 +1,14 @@
+# Tipos de dados
+
+# Numéricos, texto, booleano
+# estaticamente tipadas:
+  #precisa declarar os tipos antes de usar
+#Python é dinamicamente tipada:
+  #não precisa declarar
+
+#linguagens podem ser fracamente tipadas, como JS, onde voce combina tipos diferentes em operação e não dá problema. 
+
+#Já o Python é fortemente tipado: apesar de declrar os tipos, não deixa eu trabalhar com eles juntos. Exemplo:
+print(15 + "água") 
+#TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
